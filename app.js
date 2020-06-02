d4f516a75a23dfac58052d128d2ca18e4823e0bc
d4yonsouncloude <mattd4y@gmail.com>
2020-06-01 19:52:41 -0600

    Updates

---
+++ app.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+  el: "#app",
+  data: {
+    title: "Simple TodoList",
+    alert: "Please add a todo",
+    isAlert: false,
+    newTodo: "",
+    todos: [],
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === "") {
+        this.isAlert = true;
+        setTimeout(() => {
+          this.isAlert = false;
+        }, 2000);
+        return;
+      }
+      this.todos.push({
+        title: this.newTodo,
+        done: false,
+      });
+      this.newTodo = "";
+    },
+    removeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    allDone() {
+      this.todos.forEach((todo) => {
+        todo.done = true;
+      });
+    },
+    clearAll() {
+      this.todos = [];
+    },
+  },
+});
